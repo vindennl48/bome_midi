350666b4ba4bb6e7dc7626aba036515a105100fb
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-03-20 19:06:53 +0000

    Converts json to usable format
    Now, we can put in endpoints and links and this will convert
    that to a usable json format to later be converted into the
    saved file format of bome-midi

--- layout.py
+++ layout.py
@@ -1,51 +1,25 @@
-from ObjectContainer import ObjectContainer as oc
 from objects import n
 
-def get_body():
-    inputs = {
+endpoints = [
+    # FCB1010
+    {   'Name': 'fcb1010_btn_1',
+            'Incoming': { 'Note': 1, 'Channel': 1, 'Note Type': 'note' },
+            'Ports': [ 'Scarlett 18i20 USB', 'Bome Midi Virtual Port 1' ]},
 
-    # FCB1010 #######################################################################################################################################
-    # Foot Switches
-    "fcb1010_btn_1": { "Channel": 1, "Note":   n(1,ch=1), "Type": "note", "To":  "fx_low_cut", "Ports": [ "Scarlett 18i20 USB", "Bome Virtual Port 1" ] },
-    "fcb1010_btn_2": { "Channel": 1, "Note":   n(2,ch=1), "Type": "note", "To": "fx_low_pass", "Ports": [ "Scarlett 18i20 USB", "Bome Virtual Port 1" ] },
-    "fcb1010_btn_3": { "Channel": 1, "Note":   n(3,ch=1), "Type": "note", "To":        "stop", "Ports": [ "Scarlett 18i20 USB", "Bome Virtual Port 1" ] },
-    # Expression Pedals
-    "fcb1010_exp_1": { "Channel": 1, "Note": n(126,ch=1), "Type":   "cc", "To":  "exp_volume", "Ports": [ "Scarlett 18i20 USB", "Bome Virtual Port 1" ] },
-    "fcb1010_exp_2": { "Channel": 1, "Note": n(127,ch=1), "Type":   "cc", "To":  "exp_reverb", "Ports": [ "Scarlett 18i20 USB", "Bome Virtual Port 1" ] },
+    # ABLETON
+    {   'Name': 'fx_dist',
+            'Incoming': { 'Note': 'auto', 'Channel': 1, 'Note Type': 'cc' },
+            'Outgoing': { 'Note': 'auto', 'Channel': 1, 'Note Type': 'cc' },
+            'Ports': [ 'Bome Midi Virtual Port 1' ]}
+]
 
-    # HIPBOX ########################################################################################################################################
-    # Mitch
-#   "tosc_mitch_james": { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_james", "Ports": [ "TouchOSC Bridge" ] },
-#   "tosc_mitch_jesse": { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_jesse", "Ports": [ "TouchOSC Bridge" ] },
-#   "tosc_mitch_durms": { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_drums", "Ports": [ "TouchOSC Bridge" ] },
-#   "tosc_mitch_metro": { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_metro", "Ports": [ "TouchOSC Bridge" ] },
-#   "tosc_mitch_bass" : { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_bass",  "Ports": [ "TouchOSC Bridge" ] },
-#   "tosc_mitch_vol"  : { "Channel": 10, "Note": n(ch=10), "Type": "cc", "To": "hb_mitch_vol",   "Ports": [ "TouchOSC Bridge" ] },
-
-    }
-
-    outputs = {
-    # Effects
-    "fx_low_cut"       : { "Channel": 1, "Note": n(ch=1), "Type":   "cc", "Ports": [ "Bome Virtual Port 1" ], "Start Val":   0 },
-    "fx_low_pass"      : { "Channel": 1, "Note": n(ch=1), "Type":   "cc", "Ports": [ "Bome Virtual Port 1" ], "Start Val": 127 },
-    "exp_volume"       : { "Channel": 1, "Note": n(ch=1), "Type":   "cc", "Ports": [ "Bome Virtual Port 1" ], "Start Val":   0 },
-    "exp_reverb"       : { "Channel": 1, "Note": n(ch=1), "Type":   "cc", "Ports": [ "Bome Virtual Port 1" ], "Start Val":   0 },
-    # System
-    "stop"             : { "Channel": 1, "Note": n(ch=1), "Type": "note", "Ports": [ "Bome Virtual Port 1" ] },
-    "stop_clips"       : { "Channel": 1, "Note": n(ch=1), "Type": "note", "Ports": [ "Bome Virtual Port 1" ] },
-    "stop_clips_d"     : { "Channel": 1, "Note": n(ch=1), "Type": "note", "Ports": [ "Bome Virtual Port 1" ], "Delay": 50 },
-    "stop_play_head"   : { "Channel": 1, "Note": n(ch=1), "Type": "note", "Ports": [ "Bome Virtual Port 1" ] },
-    "stop_play_head_d" : { "Channel": 1, "Note": n(ch=1), "Type": "note", "Ports": [ "Bome Virtual Port 1" ], "Delay": 50 },
-    }
-
-    events = {
-    "stop" : { "Output": [ "stop_clips", "stop_clips_d", "stop_play_head", "stop_play_head_d" ] }
-    }
-
-    body = {
-        "Inputs"  : inputs,
-        "Outputs" : outputs,
-        "Events"  : events
-    }
-
-    return body
+links = [
+    {   'Link'   : ('fcb1010_btn_1', 'fx_dist'),
+        'Options': [
+            '// Comment',
+            '// Comment',
+            '// Comment',
+            '// Comment',
+        ],
+        'Delay'  : 200 }
+]
--- main.py
+++ main.py
@@ -1,10 +1,185 @@
-from layout import get_body
-from objects import init_variables, init_connections
+from ObjectContainer import ObjectContainer as oc
+from objects import init_variables, get_new_global_var, n, get_global_var
+from layout import endpoints, links
+from nice_json import json_print_pretty
+
+
+'''
+Summary:
+ - def run()
+ - def get_body()
+ - def get_endpoint(body, endpoint_name)
+ - def create_globals(body)
+ - def get_presets(body)
+ - def create_preset(options, body=None)
+ - def get_global_start_values()
+ - def get_translators(body)
+ - def create_options_header(link)
+ - def to_hex(num, padding=0)
+'''
+
 
 def run():
     init_variables()
     body = get_body()
-    init_connections(body)
+    create_globals(body)
 
-if __name__ == "__main__":
+    presets = get_presets(body)
+    print(json_print_pretty(presets))
+
+
+
+################################################################################
+
+
+
+def get_body():
+    body = { 'Endpoints': endpoints, 'Links': links }
+    return body
+
+
+def get_endpoint(body, endpoint_name):
+    endpoints = body['Endpoints']
+    try:
+        return (e for e in endpoints if e['Name'] == endpoint_name).next()
+    except:
+        raise Exception('Value \"{}\" does not exist!..'.format(endpoint_name))
+
+
+def create_globals(body):
+    for value in body['Endpoints']:
+        if 'Outgoing' in value and \
+           'Note Type' in value['Outgoing'] and \
+           value['Outgoing']['Note Type'] == 'cc':
+            get_new_global_var(value['Name'])
+
+
+def get_presets(body):
+    presets = [ create_preset('init') ]
+
+    presets.append( create_preset('links', body) ) 
+    return presets
+
+
+def create_preset(options, body=None):
+    result = {}
+    if options == 'init':
+        # create initial preset
+        result = {
+            'Name'   : 'Initialize',
+            'Active' : 1,
+            'Psi'    : 0,     # PresetSwitchIgnore
+            'Translators': [ get_global_start_values() ]
+        }
+    elif options == 'links':
+        # create linking preset
+        result = {
+            'Name'   : 'Links',
+            'Active' : 1,
+            'Psi'    : 0,
+            'Translators': [ get_translators(body) ]
+        }
+
+    return result
+
+
+def get_global_start_values():
+    global_vars = oc._get('global_vars')
+
+    taken_global_vars = [(g for g in global_vars if g['Taken']).next()]
+
+    rules = []
+    for g in taken_global_vars:
+        rules.append('// {}'.format(g['Taken']))
+        rules.append('{}={}'.format(g['Var'], g['Value']))
+
+    options = [ create_options_header(len(rules)) ]
+    options += rules
+
+    translator = {
+        'Name'    : 'Links',
+        'Options' : options,
+    }
+    return translator
+
+
+def get_translators(body):
+    end_points = body['Endpoints']
+    links      = body['Links']
+    result     = []
+
+    for link in links:
+        a     = link['Link'][0]
+        b     = link['Link'][1]
+        rules = link['Options']
+        translator = {}
+
+        a = get_endpoint(body, a)
+        a_note = a['Incoming']['Note'] if a['Incoming']['Note'] != 'auto' else n(a['Incoming']['Channel'])
+
+        incoming = {
+            'Desc'      : a['Name'],
+            'Note'      : a_note,
+            'Channel'   : a['Incoming']['Channel'],
+            'Note Type' : a['Incoming']['Note Type'],
+            'Ports'     : a['Ports']
+        }
+
+        options = [ create_options_header(link) ]
+        options += rules
+
+        try:
+            b = get_endpoint(body, b)
+            b_note = b['Incoming']['Note'] if b['Incoming']['Note'] != 'auto' else n(b['Incoming']['Channel'])
+
+            outgoing = {
+                'Desc'      : b['Name'],
+                'Note'      : b_note,
+                'Channel'   : b['Incoming']['Channel'],
+                'Note Type' : b['Incoming']['Note Type'],
+                'Ports'     : b['Ports']
+            }
+
+            translator = {
+                'Name'    : '{} - {}'.format(a['Name'], b['Name']),
+                'Incoming': incoming,
+                'Outgoing': outgoing,
+                'Options' : options,
+            }
+        except:
+            translator = {
+                'Name'    : '{} - {}'.format(a['Name'], b['Name']),
+                'Incoming': incoming,
+                'Options' : options,
+            }
+        result.append(translator)
+    return result
+
+
+def create_options_header(link):
+    if type(link) is not dict:
+        return 'Activ01Stop01OutO01StMa{}'.format(to_hex(link, 8))
+    else:
+        if 'Delay' in link:
+            delay = 'Dlay{}:Millis'.format(link['Delay'])
+        else:
+            delay = ''
+
+        num_rules = len(link['Options'])
+
+        return 'Activ{:02d}Stop{:02d}OutO{:02d}{}StMa{}'.format(
+            1, 0, 0, delay, to_hex(num_rules, 8)
+        )
+
+
+def to_hex(num, padding=0):
+    result = str(hex(num))[2:]
+    if padding > 0:
+        itr = padding - len(result)
+        for i in range(itr):
+            result = '0' + result
+    return result
+
+
+if __name__ == '__main__':
     run()
